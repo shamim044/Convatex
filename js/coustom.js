d6f04913356717a38c4604e0e071b8584d45a226
MehediShamim <86200822+shamim044@users.noreply.github.com>
2021-06-21 00:13:49 +0600

    convatex

---
+++ js/coustom.js
@@ -0,0 +1,39 @@
+$(document).ready(function(){
+
+	/* -- magnific-popup js --*/
+	$('.play-btn').magnificPopup({
+		type:'iframe',
+		iframe: {
+			markup: '<div class="mfp-iframe-scaler">'+
+			'<div class="mfp-close"></div>'+
+			'<iframe class="mfp-iframe" frameborder="0" allowfullscreen></iframe>'+
+			'</div>',
+
+			patterns: {
+				youtube: {
+					index: 'youtube.com/',
+
+					id: 'v=',
+
+					src: 'https://www.youtube.com/embed/%id%?autoplay=1'
+				},
+				vimeo: {
+					index: 'vimeo.com/',
+					id: '/',
+					src: '//player.vimeo.com/video/%id%?autoplay=1'
+				},
+				gmaps: {
+					index: '//maps.google.',
+					src: '%id%&output=embed'
+				}
+			},
+			srcAction: 'iframe_src',
+		}
+	});
+
+
+
+
+
+
+});
